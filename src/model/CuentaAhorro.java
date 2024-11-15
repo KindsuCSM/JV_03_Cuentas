375426d8dee986f3e688a65d604d3cc4e594c7b9
KindsuCSM <crismngk@gmail.com>
2024-11-15 20:32:20 +0100

    Update CuentaAhorro, CuentaCorriente

--- src/model/CuentaAhorro.java
+++ src/model/CuentaAhorro.java
@@ -1,6 +1,30 @@
 package model;
 
+import java.util.GregorianCalendar;
+
 public class CuentaAhorro extends Cuenta {
     private int interesAnual;
-    private
+    private Double saldoInicial;
+
+    public CuentaAhorro(int num, String titular, Double saldo, double saldoMin, GregorianCalendar fechaApertura, int interesAnual, Double saldoInicial) {
+        super(num, titular, saldo, saldoMin, fechaApertura);
+        this.interesAnual = interesAnual;
+        this.saldoInicial = saldoInicial;
+    }
+
+    public int getInteresAnual() {
+        return interesAnual;
+    }
+
+    public void setInteresAnual(int interesAnual) {
+        this.interesAnual = interesAnual;
+    }
+
+    public Double getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(Double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
 }
--- src/model/CuentaCorriente.java
+++ src/model/CuentaCorriente.java
@@ -1,4 +1,30 @@
 package model;
 
-public class CuentaCorriente {
+import java.util.GregorianCalendar;
+
+public class CuentaCorriente extends Cuenta{
+    private double comisionMantenimiento;
+    private Boolean isDomiciliado;
+
+    public CuentaCorriente(int num, String titular, Double saldo, double saldoMin, GregorianCalendar fechaApertura, double comisionMantenimiento, Boolean isDomiciliado) {
+        super(num, titular, saldo, saldoMin, fechaApertura);
+        setComisionMantenimiento(comisionMantenimiento);
+        setDomiciliado(isDomiciliado);
+    }
+
+    public double getComisionMantenimiento() {
+        return comisionMantenimiento;
+    }
+
+    public void setComisionMantenimiento(double comisionMantenimiento) {
+        this.comisionMantenimiento = comisionMantenimiento;
+    }
+
+    public Boolean getDomiciliado() {
+        return isDomiciliado;
+    }
+
+    public void setDomiciliado(Boolean domiciliado) {
+        isDomiciliado = domiciliado;
+    }
 }
