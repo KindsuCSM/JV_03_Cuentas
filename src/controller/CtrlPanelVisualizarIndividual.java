64a06d460118dba37e040ccd95ff010249f608b2
XZeroHakerX <150594778+XZeroHakerX@users.noreply.github.com>
2024-11-27 02:28:56 +0100

    Update Pantalla Individual
    Creacion de dos paneles intercambiables para mostrar informacion de un tipo de cuenta u otra.
    Implementacion de codigo necesario para control de las misma paneles y mostrado de informacion.
    Implementacion para la anulacion de botones por posicion.
    Añadido boton de Pirmero y Ultimo.

--- src/controller/CtrlPanelVisualizarIndividual.java
+++ src/controller/CtrlPanelVisualizarIndividual.java
@@ -2,52 +2,132 @@ package controller;
 
 import javax.swing.DefaultListModel;
 
-import model.Cuenta;
-import model.Lista;
-import model.Nodo;
+import model.*;
 import view.PanelVisualizarIndividual;
 import view.PanelVisualizarTodo;
 
 public class CtrlPanelVisualizarIndividual {
-	private static DefaultListModel<String> listaMostrar = new DefaultListModel<>();
-	
-	public static void mostrarSiguiente() {
-		
-        Lista listaCuentas = CtrlLista.getListaCuentas(); 
-        
-        Nodo actual = listaCuentas.getPrimero();
-        
-        Cuenta cuenta = (Cuenta) actual.getValor();
-            
-        String infoCuenta = cuenta.toString();
-        listaMostrar.addElement(infoCuenta);
-        
-        actual = actual.getSiguiente(); 
-        
-        PanelVisualizarIndividual.list.setModel(listaMostrar); 
-        
+
+//Declaramos los atributos necesarios:
+    private static Lista listaCuentas = CtrlLista.getListaCuentas();
+    private static Nodo actual;
+    private static CuentaAhorro cca;
+    private static CuentaCorriente ccc;
+
+    private static boolean quieroUltimo = false, quieroPrimero = false;
+
+
+//Metodo para mostrar siguiente elemento:
+    public static void mostrarSiguiente() {
+
+//Nulo al comenzar, sino comprueba si se busqua el primer o el ultimo elemento, y sino asigna siguiente():
+        if (actual == null) {
+            actual = listaCuentas.getPrimero();
+        } else if(quieroUltimo == true) {
+            actual = listaCuentas.getUltimo();
+        }else {
+            actual = actual.getSiguiente();
+        }
+
+//Activa o desactiva boton anterior:
+        if(actual.getAnterior()==null){
+            PanelVisualizarIndividual.btnAnterior.setEnabled(false);
+            PanelVisualizarIndividual.btnPrimero.setEnabled(false);
+        }else{
+            PanelVisualizarIndividual.btnAnterior.setEnabled(true);
+            PanelVisualizarIndividual.btnPrimero.setEnabled(true);
+        }
+
+//Desactiva boton siguiente cuando llega al final:
+        if(actual == listaCuentas.getUltimo()){
+            PanelVisualizarIndividual.btnSiguiente.setEnabled(false);
+            PanelVisualizarIndividual.btnUltimo.setEnabled(false);
+        }
+
+        mostrarCuenta();
 
     }
-        
-       
+
+
+//Metodo para mostrar elemento anterior:
 	public static void mostrarAnterior() {
-        
-        Lista listaCuentas = CtrlLista.getListaCuentas();
-        
-        Nodo actual = listaCuentas.getPrimero();
-        
-        Cuenta cuenta = (Cuenta) actual.getValor();
-            
-        String infoCuenta = cuenta.toString(); 
-        listaMostrar.addElement(infoCuenta); 
-        
-            
-        actual = actual.getSiguiente(); 
-        PanelVisualizarIndividual.list.setModel(listaMostrar); 
-        
-	}
-	
-	public void calcular() {
+
+//Asigna anterior al nodo:
+        if(quieroPrimero == true) {
+            actual = listaCuentas.getPrimero();
+        }else{
+            actual = actual.getAnterior();
+        }
+
+//Proceso contrario para activar o desactivar botones:
+        if(actual.getAnterior()==null){
+            PanelVisualizarIndividual.btnAnterior.setEnabled(false);
+            PanelVisualizarIndividual.btnPrimero.setEnabled(false);
+        }else{
+            PanelVisualizarIndividual.btnAnterior.setEnabled(true);
+            PanelVisualizarIndividual.btnPrimero.setEnabled(true);
+        }
+
+        if(actual != listaCuentas.getUltimo()){
+            PanelVisualizarIndividual.btnSiguiente.setEnabled(true);
+            PanelVisualizarIndividual.btnUltimo.setEnabled(true);
+        }
+
+        mostrarCuenta();
+
+    }
+
+
+//Metodos para cambiar variables a true si se busca primero o ultimo:
+    public static void mostrarUltimo(){
+        quieroUltimo = true;
+        mostrarSiguiente();
+
+    }
+
+    public static void mostrarPrimero(){
+        quieroPrimero = true;
+        mostrarAnterior();
+    }
+
+
+//Intenta conversion a cuenta ahorro y si no puede, hace la conversion a cuenta corriente, luego manda
+// a cambiar el panel según sea uno u otro y setea los datos:
+    private static void mostrarCuenta(){
+
+        try{
+            cca = (CuentaAhorro) actual.getValor();
+            PanelVisualizarIndividual.cambiarPanel(1);
+            PanelVisualizarIndividual.txtTitularCA.setText(cca.getTitular());
+            PanelVisualizarIndividual.txtNumCuentaCA.setText(cca.getNum().toString());
+            PanelVisualizarIndividual.txtSaldoCA.setText(cca.getSaldo().toString());
+            PanelVisualizarIndividual.txtFechaAperturaCA.setText(cca.getFechaApertura().toString());
+            PanelVisualizarIndividual.txtInteresAnualCA.setText(String.valueOf(cca.getInteresAnual()));
+            PanelVisualizarIndividual.txtSaldoInicialCA.setText(String.valueOf(cca.getSaldoInicial()));
+            PanelVisualizarIndividual.txtSaldoMinCA.setText(String.valueOf(cca.getSaldoMin()));
+
+
+        }catch(Exception e){
+            ccc = (CuentaCorriente) actual.getValor();
+            PanelVisualizarIndividual.cambiarPanel(2);
+            PanelVisualizarIndividual.txtTitular.setText(ccc.getTitular());
+            PanelVisualizarIndividual.txtNumCuenta.setText(ccc.getNum().toString());
+            PanelVisualizarIndividual.txtSaldo.setText(ccc.getSaldo().toString());
+            PanelVisualizarIndividual.txtFechaApertura.setText(ccc.getFechaApertura().toString());
+            PanelVisualizarIndividual.txtComisionMantenimiento.setText(String.valueOf(ccc.getComisionMantenimiento()));
+            PanelVisualizarIndividual.txtDomiciliado.setText(ccc.getDomiciliado().toString());
+            PanelVisualizarIndividual.txtSaldoMin.setText(String.valueOf(ccc.getSaldoMin()));
+
+        }
+
+        quieroUltimo = false;
+        quieroPrimero = false;
+    }
+
+
+//Metodo para calcular . . .:
+    public void calcular() {
 		
 	}
+
 }
--- src/view/PanelVisualizarIndividual.java
+++ src/view/PanelVisualizarIndividual.java
@@ -1,46 +1,199 @@
 package view;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import controller.CtrlPanelVisualizarIndividual;
 
-import java.awt.BorderLayout;
-import javax.swing.JList;
-import javax.swing.JButton;
+import java.awt.*;
+import java.io.Serial;
 
 public class PanelVisualizarIndividual extends JPanel {
 
+	@Serial
 	private static final long serialVersionUID = 1L;
-	private JPanel panel;
-	public static JButton btnAnterior, btnSiguiente, btnCalcular;
-	public static JList list;
-	
 
-	/**
-	 * Create the panel.
-	 */
+//Declaracion paneles:
+	public static JPanel panel, panelVistaCA, panelVistaCC;
+
+//Declaraciones componentes panel CA:
+	public static JTextField txtNumCuentaCA, txtTitularCA, txtSaldoCA, txtSaldoMinCA, txtFechaAperturaCA, txtInteresAnualCA, txtSaldoInicialCA;
+	public static JLabel lblNumCuentaCA, lblTitularCA, lblSaldoCA, lblSaldoMinCA, lblFechaAperturaCA, lblInteresAnualCA, lblSaldoInicialCA;
+
+//Declaraciones componentes Panel CC:
+	public static JTextField txtNumCuenta, txtTitular, txtSaldo, txtSaldoMin, txtFechaApertura, txtComisionMantenimiento, txtDomiciliado;
+	public static JLabel blank, lblNumCuenta, lblTitular, lblSaldo, lblSaldoMin, lblFechaApertura, lblComisionMantenimiento, lblDomiciliado;
+
+//Declaracion botones:
+	public static JButton btnPrimero, btnUltimo, btnAnterior, btnSiguiente, btnCalcular;
+
+//Declaracion de contexto unico:
+	public static PanelVisualizarIndividual context;
+
+
+
+//Crear panel:
 	public PanelVisualizarIndividual() {
+
+//Iniciamos Frame:
 		setLayout(new BorderLayout(0, 0));
 		addComponents();
 		setListeners();
 
+//Iniciamos contexto y mostramos el primer elemento:
+		context = this;
+		CtrlPanelVisualizarIndividual.mostrarSiguiente();
+
 	}
+
+//Metodo para añadir componentes:
 	private void addComponents() {
+
+//Panel Botones parte sur:
 		panel = new JPanel();
+		panel.setLayout(new GridLayout(1,0,15,0));
 		add(panel, BorderLayout.SOUTH);
-		
+
+		btnPrimero = new JButton("Primero");
+		panel.add(btnPrimero);
+
 		btnAnterior = new JButton("Anterior");
 		panel.add(btnAnterior);
 		
 		btnSiguiente = new JButton("Siguiente");
 		panel.add(btnSiguiente);
+
+		btnUltimo = new JButton("Ultimo");
+		panel.add(btnUltimo);
 		
 		btnCalcular = new JButton("Calcular");
 		panel.add(btnCalcular);
-		
-		list = new JList();
-		add(list, BorderLayout.CENTER);
+
+
+//Panel Vista individual CA:
+
+		panelVistaCA = new JPanel();
+		panelVistaCA.setLayout(new GridLayout(0, 2, 10, 10));
+
+		lblNumCuentaCA = new JLabel("Numero de cuenta:");
+		panelVistaCA.add(lblNumCuentaCA);
+
+		txtNumCuentaCA = new JTextField();
+		panelVistaCA.add(txtNumCuentaCA);
+		txtNumCuentaCA.setColumns(10);
+		txtNumCuentaCA.setEditable(false);
+
+		lblTitularCA = new JLabel("Titular: ");
+		panelVistaCA.add(lblTitularCA);
+
+		txtTitularCA = new JTextField();
+		panelVistaCA.add(txtTitularCA);
+		txtTitularCA.setColumns(10);
+		txtTitularCA.setEditable(false);
+
+		lblSaldoCA = new JLabel("Saldo:");
+		panelVistaCA.add(lblSaldoCA);
+
+		txtSaldoCA = new JTextField();
+		panelVistaCA.add(txtSaldoCA);
+		txtSaldoCA.setColumns(10);
+		txtSaldoCA.setEditable(false);
+
+		lblSaldoMinCA = new JLabel("Saldo mínimo:");
+		panelVistaCA.add(lblSaldoMinCA);
+
+		txtSaldoMinCA = new JTextField();
+		panelVistaCA.add(txtSaldoMinCA);
+		txtSaldoMinCA.setColumns(10);
+		txtSaldoMinCA.setEditable(false);
+
+		lblFechaAperturaCA = new JLabel("Fecha apertura (DD-MM-YYYY):");
+		panelVistaCA.add(lblFechaAperturaCA);
+
+		txtFechaAperturaCA = new JTextField();
+		panelVistaCA.add(txtFechaAperturaCA);
+		txtFechaAperturaCA.setColumns(10);
+		txtFechaAperturaCA.setEditable(false);
+
+		lblInteresAnualCA = new JLabel("Interés anual:");
+		panelVistaCA.add(lblInteresAnualCA);
+
+		txtInteresAnualCA = new JTextField();
+		panelVistaCA.add(txtInteresAnualCA);
+		txtInteresAnualCA.setColumns(10);
+		txtInteresAnualCA.setEditable(false);
+
+		lblSaldoInicialCA = new JLabel("Saldo inicial:");
+		panelVistaCA.add(lblSaldoInicialCA);
+
+		txtSaldoInicialCA = new JTextField();
+		panelVistaCA.add(txtSaldoInicialCA);
+		txtSaldoInicialCA.setColumns(10);
+		txtSaldoInicialCA.setEditable(false);
+
+//Panel Vista individual CC:
+
+		panelVistaCC = new JPanel();
+		panelVistaCC.setLayout(new GridLayout(0, 2, 10, 10));
+
+		lblNumCuenta = new JLabel("Numero de cuenta:");
+		panelVistaCC.add(lblNumCuenta);
+
+		txtNumCuenta = new JTextField();
+		panelVistaCC.add(txtNumCuenta);
+		txtNumCuenta.setColumns(10);
+		txtNumCuenta.setEditable(false);
+
+		lblTitular = new JLabel("Titular: ");
+		panelVistaCC.add(lblTitular);
+
+		txtTitular = new JTextField();
+		panelVistaCC.add(txtTitular);
+		txtTitular.setColumns(10);
+		txtTitular.setEditable(false);
+
+		lblSaldo = new JLabel("Saldo:");
+		panelVistaCC.add(lblSaldo);
+
+		txtSaldo = new JTextField();
+		panelVistaCC.add(txtSaldo);
+		txtSaldo.setColumns(10);
+		txtSaldo.setEditable(false);
+
+		lblSaldoMin = new JLabel("Saldo mínimo:");
+		panelVistaCC.add(lblSaldoMin);
+
+		txtSaldoMin = new JTextField();
+		panelVistaCC.add(txtSaldoMin);
+		txtSaldoMin.setColumns(10);
+		txtSaldoMin.setEditable(false);
+
+		lblFechaApertura = new JLabel("Fecha apertura (DD-MM-YYYY):");
+		panelVistaCC.add(lblFechaApertura);
+
+		txtFechaApertura = new JTextField();
+		panelVistaCC.add(txtFechaApertura);
+		txtFechaApertura.setColumns(10);
+		txtFechaApertura.setEditable(false);
+
+		lblComisionMantenimiento = new JLabel("Comisión por mantenimiento: ");
+		panelVistaCC.add(lblComisionMantenimiento);
+
+		txtComisionMantenimiento = new JTextField();
+		panelVistaCC.add(txtComisionMantenimiento);
+		txtComisionMantenimiento.setColumns(10);
+		txtComisionMantenimiento.setEditable(false);
+
+		lblDomiciliado = new JLabel("Está domiciliado: ");
+		panelVistaCC.add(lblDomiciliado);
+
+		txtDomiciliado = new JTextField();
+		panelVistaCC.add(txtDomiciliado);
+		txtDomiciliado.setColumns(10);
+		txtDomiciliado.setEditable(false);
+
 	}
+
+//Metodo para añadir listeners:
 	private void setListeners() {
 		btnAnterior.addActionListener(e -> {
 			CtrlPanelVisualizarIndividual.mostrarAnterior();
@@ -48,9 +201,36 @@ public class PanelVisualizarIndividual extends JPanel {
 		btnSiguiente.addActionListener(e -> {
 			CtrlPanelVisualizarIndividual.mostrarSiguiente();
 		});
+
+		btnPrimero.addActionListener(e -> {
+			CtrlPanelVisualizarIndividual.mostrarPrimero();
+		});
+		btnUltimo.addActionListener(e -> {
+			CtrlPanelVisualizarIndividual.mostrarUltimo();
+		});
+
 		btnCalcular.addActionListener(e -> {
 			
 		});
+	}
+
+//Metodo para el cambio de panel, según sea un tipo de cuenta u otro:
+	public static void cambiarPanel(int opcion){
+
+		if(opcion == 1){
+			context.add(panelVistaCA, BorderLayout.CENTER);
+			panelVistaCC.setVisible(false);
+			panelVistaCA.setVisible(true);
+			context.revalidate();
+			context.repaint();
+		}else if(opcion == 2){
+			context.add(panelVistaCC, BorderLayout.CENTER);
+			panelVistaCA.setVisible(false);
+			panelVistaCC.setVisible(true);
+			context.revalidate();
+			context.repaint();
+		}
+
 	}
 
 }
